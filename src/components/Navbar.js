ef23ccfde2057630f01e8140eaa2ba59cc121c98
Aleksandra Dahlin <aleksandra.dahlin@gmail.com>
2023-04-24 17:49:00 +0200

    fix navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../images/logo.svg'
+import { pageLinks, socialLinks } from '../data';
 
 function Navbar() {
     return (
@@ -12,42 +13,26 @@ function Navbar() {
                     </button>
                 </div>
                 <ul className="nav-links" id="nav-links">
-                    <li>
-                        <a href="#home" className="nav-link"> home </a>
-                    </li>
-
-                    <li>
-                        <a href="#about" className="nav-link"> about </a>
-                    </li>
-
-                    <li>
-                        <a href="#services" className="nav-link"> services </a>
-                    </li>
-
-                    <li>
-                        <a href="#tours" className="nav-link"> tours</a>
-                    </li>
+                    {pageLinks.map(link => (
+                        <li>
+                            <a key={link.id} href={link.href} className="nav-link"> {link.text} </a>
+                        </li>
+                    ))}
                 </ul>
 
                 <ul className="nav-icons">
-                    <li>
-                        <a href="https://www.twitter.com" target="_blank" rel="noreferrer" className="nav-icon"
-                        ><i className="fab fa-facebook"></i
-                        ></a>
-                    </li>
-                    <li>
-                        <a href="https://www.twitter.com" target="_blank" rel="noreferrer" className="nav-icon"
-                        ><i className="fab fa-twitter"></i
-                        ></a>
-                    </li>
-                    <li>
-                        <a href="https://www.twitter.com" target="_blank" rel="noreferrer" className="nav-icon"
-                        ><i className="fab fa-squarespace"></i
-                        ></a>
-                    </li>
+                    {socialLinks.map(link => {
+                        const { id, href, className } = link;
+                        return (
+                            <li>
+                                <a key={id} href={href} target="_blank" rel="noreferrer" className="nav-icon"
+                                ><i className={className}></i>
+                                </a>
+                            </li>)
+                    })}
                 </ul>
             </div>
-        </nav>
+        </nav >
     );
 }
 
